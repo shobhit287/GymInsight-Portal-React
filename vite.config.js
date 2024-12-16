25bbbc8aecfd67ddcf79f494c62e17a9fa2223a9
aditiroygithub <aditi.roy@aksconsultingservices.com>
2024-12-17 01:17:17 +0530

    updare route path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
   server: {
     port: 3000,
